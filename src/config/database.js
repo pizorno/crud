61eb2357f79e57498394ca14d6046b776d036a65
Eduardo Pizorno <pizorno@gmail.com>
2022-01-23 21:36:05 -0300

    Configurações DB

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'kesavan.db.elephantsql.com',
+  username: 'afadxgvb',
+  password: 'kHbsvFeBCZX1gMwoaw-eg9Yy3AlwrqHm',
+  database: 'afadxgvb',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+};
